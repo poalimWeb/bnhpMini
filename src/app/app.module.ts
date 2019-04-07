7436bffacfaee1ac738b0cd2274d3826f6300328
ShahafShuhamy <Shahaf761@gmail.com>
2019-04-07 15:13:59 +0300

    add fonts, done with main tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBnhpComponent } from './nav-bnhp/nav-bnhp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBnhpComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nav-bnhp/nav-bnhp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bnhp',
+  templateUrl: './nav-bnhp.component.html',
+  styleUrls: ['./nav-bnhp.component.css']
+})
+export class NavBnhpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
