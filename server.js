459e441447e8ca3beaec7af0a45279cce8422f77
ShahafShuhamy <Shahaf761@gmail.com>
2019-04-11 15:05:01 +0300

    sd

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/bnhp-app'));
+app.use(express.static(__dirname + '/dist/bnhpApp'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/bnhp-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/bnhpApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
